29247865dbfb5b05d31e6c1155958c48cf86bf8a
yangmaosheng66 <et4545@126.com>
2018-05-24 19:44:20 +0800

    commit test1

---
+++ src/com/chapter1/Test1_6.java
@@ -0,0 +1,9 @@
+package com.chapter1;
+
+public class Test1_6 {
+    public static void main(String[] args){
+        System.out.println("123");
+        System.out.println("456");
+        System.out.println("789");
+    }
+}
