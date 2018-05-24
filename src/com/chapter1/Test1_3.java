7ff4cb7c94326ac007b7f6b979ffc93bd2dcfca0
yangmaosheng66 <et4545@126.com>
2018-05-24 19:34:02 +0800

    commit test1

--- src/com/chapter1/Test1_3.java
+++ src/com/chapter1/Test1_3.java
@@ -6,5 +6,6 @@ public class Test1_3 {
         System.out.println("这是测试1");
         System.out.println("这是测试2");
         System.out.println("这是测试3");
+        System.out.println("这是测试4");
     }
 }
