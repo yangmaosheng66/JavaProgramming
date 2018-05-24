8390bb0f3c45709040ec1d18baae58d88ec06bae
yangmaosheng66 <et4545@126.com>
2018-05-24 19:09:24 +0800

    commit test1

--- src/com/chapter1/Main.java
+++ src/com/chapter1/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("你好！");
+        System.out.println("你好！hello");
         System.out.println("java!");
+        System.out.println("大家好");
     }
 }
