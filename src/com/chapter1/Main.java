76a2960d030d4b40ea9fb63868d7856eb6df19bc
yangmaosheng66 <et4545@126.com>
2018-05-24 18:32:30 +0800

    Initial commit

---
+++ src/com/chapter1/Main.java
@@ -0,0 +1,10 @@
+package com.chapter1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("你好！");
+        System.out.println("java!");
+    }
+}
