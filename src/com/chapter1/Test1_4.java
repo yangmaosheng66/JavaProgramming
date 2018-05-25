d0c3c66599c9354a640ac33714f53cf42380ed0e
yms <et4545@126.com>
2018-05-25 11:27:11 +0800

    commit test

--- src/com/chapter1/Test1_4.java
+++ src/com/chapter1/Test1_4.java
@@ -4,5 +4,6 @@ public class Test1_4 {
     public static void main(String[] args){
         System.out.println("final test");
         System.out.println("final test1");
+        System.out.println("456789987");
     }
 }
---
+++ src/com/chapter1/Test1_5.java
@@ -0,0 +1,8 @@
+package com.chapter1;
+
+public class Test1_5 {
+    public static void main(String[] args){
+        System.out.println("14234");
+        System.out.println("jhgudh");
+    }
+}
